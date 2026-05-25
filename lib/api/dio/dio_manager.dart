69af0240105e58ed4f9241e7ee29328eb488b171
PC-ORBIT <PC-ORBIT@DESKTOP-IK6P0R2>
2026-05-25 15:13:42 +0300

    fix(api): fix malformed baseUrl and redundant endpoint paths

--- lib/api/dio/dio_manager.dart
+++ lib/api/dio/dio_manager.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:news_app/api/api_constants.dart';
 import 'package:news_app/api/api_endpoints.dart';
+import 'package:news_app/api/dio/dio_interceptor.dart';
 import 'package:news_app/model/news_response.dart';
 import 'package:news_app/model/source_response.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -9,7 +10,10 @@ class DioManager {
   final Dio dio = Dio(
     BaseOptions(
       baseUrl: "https://newsapi.org/v2/",
-      queryParameters: {'apiKey': ApiConstants.apiKey},
+      // queryParameters: {'apiKey': ApiConstants.apiKey},
+      // headers: {
+      //   'X-Api-Key': ApiConstants.apiKey,
+      // },
       receiveTimeout: Duration(seconds: 30),
       connectTimeout: Duration(seconds: 30),
       receiveDataWhenStatusError: true,
@@ -29,15 +33,22 @@ class DioManager {
   //     ),
   //   );
   // }
-  //todo:another way instead logInterceptor
+  //todo:another way instead of logInterceptor
   DioManager() {
+    dio.interceptors.add(DioInterceptor());
     dio.interceptors.add(
       PrettyDioLogger(
+        request: true,
         requestHeader: true,
         requestBody: true,
         responseBody: true,
         responseHeader: false,
         error: true,
+        compact: false,
+        maxWidth: 120,
+        logPrint: (object) {
+          print(object.toString());
+        },
       ),
     );
   }
@@ -50,6 +61,9 @@ class DioManager {
       );
       var json = response.data;
       return SourceResponse.fromJson(json);
+    } on DioException catch (e) {
+      print('DioException: ${e.message}');
+      rethrow;
     } catch (e) {
       rethrow;
     }
